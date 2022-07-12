6cafbba6fb3433649db827912d4b6bf036a1c098
Lijingwei <lijw@miduonet.com>
2022-07-12 09:52:37 +0800

    fix: 修复自适应bug

--- src/flexible.js
+++ src/flexible.js
@@ -1,26 +1,40 @@
-const { throttle } = require("./utils.js")
+// const { throttle } = require("./utils.js")
 
-function resize(box) {
+function enhance(box) {
+  let tempBox = box
   let width = window.innerWidth
   let height = window.innerHeight
-  box.style.width = width + "px"
-  box.style.height = height + "px"
-  window.addEventListener("resize", throttle(resize, 300))
-
-  // 1080/1920 = 0.5625
-  if(window.screen.height / window.screen.width === 0.5625) {
-    height = width * 0.5625
-  } else {
-    if(height / width < 0.5625) {
-      width = height / 0.5625
+  tempBox.style.width = width + "px"
+  tempBox.style.height = height + "px"
+  
+  function resize() {
+    width = window.innerWidth
+    height = window.innerHeight
+    tempBox.style.width = width + "px"
+    tempBox.style.height = height + "px"
+    // 1080/1920 = 0.5625
+    if(window.screen.height / window.screen.width === 0.5625) {
+      height = width * 0.5625
     } else {
-      height = width / 0.5625
+      if(height / width < 0.5625) {
+        width = height / 0.5625
+      } else {
+        height = width / 0.5625
+      }
     }
+    if(width <= 1024) {
+      width = 1024
+      height = 576
+    }
+    tempBox.style.transform = `scale(
+      ${width / window.screen.width},
+      ${height / window.screen.height}
+    )`
+    window.location.reload()
   }
-  box.style.transform = `scale(
-    ${width / window.screen.width},
-    ${height / window.screen.height}
-  )`
+
+  window.addEventListener("resize", throttle(resize, 3000))
 }
 
-module.exports = resize
+
+// module.exports = enhance
